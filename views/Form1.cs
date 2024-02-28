a837cc24575c0b0ca4c7b6321efeddd3af56df6e
Jhoni <jhoni@lloadd.com.br>
2024-02-28 10:39:56 -0300

    first commit

---
+++ views/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Poc.Crud
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void toolStripNovo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripSalvar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripExcluir_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBoxItens_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
